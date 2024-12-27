c432b1a67aa71c64e52b428e79f2d81c80c75893
amokrzycki <amokrzycki@proton.me>
2024-12-27 13:49:50 +0100

    Products methods update

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from '../entities/product.entity';
+import { Like } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
@@ -16,6 +17,30 @@ export class ProductsService {
 
   findOne(id: number): Promise<Product> {
     return this.productsRepository.findOneBy({ product_id: id });
+  }
+
+  findByCategory(category: string): Promise<Product[]> {
+    return this.productsRepository.findBy({ category });
+  }
+
+  async findLikeName(name: string): Promise<Product[]> {
+    return this.productsRepository.find({
+      where: {
+        name: Like(`%${name}%`),
+      },
+    });
+  }
+
+  async findLikeNameInCategory(
+    category: string,
+    name: string,
+  ): Promise<Product[]> {
+    return this.productsRepository.find({
+      where: {
+        name: Like(`%${name}%`),
+        category,
+      },
+    });
   }
 
   create(product: Partial<Product>): Promise<Product> {
